136ecf714da9f537614d39ba0601724d21684e8f
Giovana da Silveira <102772845+giovana-ds@users.noreply.github.com>
2022-10-20 00:54:13 +0000

    Aprendi a criar variáveis e exibi-las na tela!

---
+++ main.py
@@ -0,0 +1,17 @@
+# Primeiro projeto Python :)
+#
+# comentário em linha
+'''
+comentário em bloco
+'''
+# print() = comando de saída
+print("Hello world!")
+
+# para guardar String (frase)
+nome = "Giovana da Silveira"
+# para guardar número inteiro
+quantidade = 28
+
+# exibir
+print(nome)
+print(quantidade)
