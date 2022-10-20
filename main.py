6b2fb18edb1035ef63d462bf8c71adbbbf2fca47
Giovana da Silveira <102772845+giovana-ds@users.noreply.github.com>
2022-10-20 01:19:39 +0000

    Aprendi a exibir de 3 formas diferentes!

--- main.py
+++ main.py
@@ -7,11 +7,19 @@ comentário em bloco
 # print() = comando de saída
 print("Hello world!")
 
-# para guardar String (frase)
+# para guardar String (frase):
 nome = "Giovana da Silveira"
-# para guardar número inteiro
+# para guardar número inteiro:
 quantidade = 28
 
-# exibir
+# exibindo:
 print(nome)
 print(quantidade)
+
+# quando quiser exibir a frase "A quantidade é " completando com o conteúdo da variável "quantidade":
+print("A quantidade é "+str(quantidade))
+print(f"A quantidade é {quantidade}")
+print("A quantidade é {}".format(quantidade))
+
+# quando quiser exibir uma frase com duas variáveis usando o ".format":
+print("Meu nome é {} e tenho {} maçãs".format(nome, quantidade))
