ae237937de7413ea2f65ccee502aaacc931a1495
Giovana da Silveira <102772845+giovana-ds@users.noreply.github.com>
2022-11-17 02:40:19 +0000

    incluir quaisquer heróis/vilões que o usuário quiser

--- aula4_2022_11_16c.py
+++ aula4_2022_11_16c.py
@@ -1,11 +1,11 @@
 import sqlite3
 
-#PASSOS 2 e 3 VIRARÃO função conectar()
+# função conectar()
 def conectar():
-  #2o. passo: Vamos estabelecer uma conexão com o banco de dados
+  # estabelecer uma conexão com o banco de dados
   conexao = sqlite3.connect("dc_universe.db")
 
-  #3o. passo: criar um objeto do tipo cursor
+  # criar um objeto do tipo cursor
   cursor = conexao.cursor()
 
   return conexao, cursor 
